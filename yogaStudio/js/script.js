ace584ebb656705b69c2a61b67f07949b8e63bcf
jenniferHarper <52581347+jenniferharper@users.noreply.github.com>
2022-03-21 16:01:46 +1300

    nice and tidy

--- yogaStudio/js/script.js
+++ yogaStudio/js/script.js
@@ -1,20 +1,153 @@
 console.clear;
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, SplitText,MorphSVGPlugin, MotionPathPlugin, DrawSVGPlugin);
 
-// ScrollTrigger.matchMedia({  
-//   //tablet/mobile
-//   "(max-width: 992px)": function() {
+ScrollTrigger.matchMedia({  
+//tablet/mobile
+ "(max-width: 1023px)": function() {
 
-//   },
-//   "(min-width: 1400px)": function() {
+},
+"(min-width: 1024px)": function() {
 
  
-//   },
+},
 
-//   "all": function() {
+ "all": function() {
+    /////---------- Navigation
+    //////hamburger and menu changes/////
+    $('.navbar-toggler').click(function(){
+      $(this).toggleClass('open');
+    });
 
-//   }	
-// });
+
+    $('.nav-item a').click(function() {
+      $('.navbar-toggler').removeClass('open');
+    });
+
+    $('.nav-link').on('click',function() {
+      $('.navbar-collapse').collapse('hide');
+    });
+
+    ////adds bg color when start scrolling
+    var scrollUp = document.querySelector('.navbar');
+    ScrollTrigger.create({
+      id:'scrolling-down',
+        toggleClass: {className: 'nav--scrolled', targets: scrollUp,},
+    });
+
+    //// background image pan hero
+    gsap.timeline({
+      repeat:-1, yoyo:true,
+    })
+    .to('.bg-img-hero', {
+      backgroundPosition:'100% 100%',
+      duration:10, ease:'none',
+    },0)
+
+
+    ////// headings animation
+      var text = gsap.utils.toArray(".aniText");
+      text.forEach((el) => {
+          var splitWords = new SplitText(el, {type: "words,chars"});
+          chars = splitWords.words;
+
+          var splitTimeline = gsap.timeline({
+            scrollTrigger: {
+              trigger: el,
+              start: "top bottom-=100",
+              end: "bottom top",
+              toggleActions: "play none none none",
+            }
+          });
+          splitTimeline.from(chars, {
+              opacity:0,
+              duration: 1,
+              yPercent: 100,
+              ease: "back.out",
+              stagger: 0.02
+          });
+      });
+
+
+    ////// text around arch/////
+    gsap.to(".drawText",10,{attr:{startOffset:'105%'},repeat:-1, ease:'none'});
+
+    //// set image to center on masked images
+    gsap.set('#about-panning, #horz-one-panning, #horz-two-panning', {	
+      attr:{ x:'-50%' },
+    })
+
+    ////horizonl scroll section////
+    const horizontalSections = gsap.utils.toArray('section.horizontal')
+    horizontalSections.forEach(function (sec, i) {	  
+      var thisPinWrap = sec.querySelector('.pin-wrap');
+      var thisAnimWrap = thisPinWrap.querySelector('.animation-wrap');  
+      var getToValue = () => -(thisAnimWrap.scrollWidth - window.innerWidth); 
+
+      gsap.fromTo(thisAnimWrap, { 
+        x: () => thisAnimWrap.classList.contains('animation-wrap') ? 0 : getToValue() }, { 
+        x: () => thisAnimWrap.classList.contains('animation-wrap') ? getToValue() : 0, 
+        ease: "none",
+        scrollTrigger: {
+          trigger: sec,		
+          start: "top top",
+          end: () => "+=" + thisAnimWrap.scrollWidth,
+          pin: true,  
+          pinReparent: true, 
+          invalidateOnRefresh: true,
+          scrub: true,
+        }
+      });
+    });	
+
+    /////////////////////////////marquee text feature
+    r = 100;
+    adjustJank = 4;
+    const scrollElems = document.querySelectorAll('.marquee-container p');
+
+    function adjustTween(obj, d) {
+      // Get the progress of the previous tween if it exists
+      let progress = 0;
+      if(obj.tween) {
+        progress = obj.tween.progress();
+        // Kill the previous tween
+        obj.tween.kill();
+      }
+
+      var t = d/r;
+      obj.tween = gsap.fromTo(obj.parentElement, {x: 0}, {
+        duration: t,
+        x: "-"+(d+adjustJank), 
+        ease: 'linear',
+        repeat: -1,
+      }).progress(progress); 
+      // Set the progress of the new tween to the same value of
+      // the previous tween (if it exists) before it was killed
+    }
+
+    // Set up for what appears to be an seamless stream of text
+    // This could go in an init() function
+    scrollElems.forEach((obj, i) => {
+      var d = obj.offsetWidth;
+      var parent = obj.parentElement;
+      var clone = obj.cloneNode(true);
+      parent.appendChild(clone);
+      gsap.set(parent.parentElement, {width: d});      
+      adjustTween(obj, d);
+    });
+
+    // Adjust widths and tweens on resize
+    window.addEventListener("resize", () => {
+      scrollElems.forEach((obj, i) => {
+        var d = obj.offsetWidth;
+        var parent = obj.parentElement;
+        gsap.set(parent.parentElement, {width: d});
+        adjustTween(obj, d);
+      });
+    });
+    // =============================
+
+  }	
+});
 
 
 // var isMobile = window.matchMedia("only screen and (max-width: 760px)"),
@@ -25,196 +158,10 @@ gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, SplitText,MorphSVGPlugin, Mot
 // } else {
 //     // things i want on desk only
 //     console.log("no touch support");
-//     // ///moving arch background/////
-//     // gsap.timeline({repeat:-1, yoyo:true})
-//     // .to('#hero-panning', {
-//     //   objectPosition:'90% 0',
-//     //   duration:10, ease:'none',
-//     //   repeat:1, yoyo:true
-//     // },0)
-
-//     ///moving arch background/////
-
 // }
-////////////////////////////////////////////
-
-gsap.timeline({
-  repeat:-1, yoyo:true,
-    scrollTrigger: {
-      trigger: '.hero',
-      start: "top top+=100",
-      end:"center center",
-      // toggleActions: 'play none none reverse',
-      markers: true,
-    }
-})
-.to('.bg-img-test', {
-  backgroundPosition:'100% 100%',
-  duration:10, ease:'none',
-},0)
-
-/////---------- Navigation
-//////hamburger and menu changes/////
-$('.navbar-toggler').click(function(){
-	$(this).toggleClass('open');
-});
-
-
-$('.nav-item a').click(function() {
-   $('.navbar-toggler').removeClass('open');
-});
-
-$('.nav-link').on('click',function() {
-  $('.navbar-collapse').collapse('hide');
-});
-
-
-// text around arch/////
-gsap.to(".drawText",10,{attr:{startOffset:'105%'},repeat:-1, ease:'none'});
-
-
-////////////////////////////////////////////////////////////////
-
-
-// set image to center
-gsap.set('#about-panning, #horz-one-panning, #horz-two-panning', {	
-  attr:{ x:'-50%' },
-
-})
-
-
-///////////////////////////horizonl scroll section/////////////////////////////////////
-
-const horizontalSections = gsap.utils.toArray('section.horizontal')
-
-horizontalSections.forEach(function (sec, i) {	  
-  var thisPinWrap = sec.querySelector('.pin-wrap');
-  var thisAnimWrap = thisPinWrap.querySelector('.animation-wrap');  
-  var getToValue = () => -(thisAnimWrap.scrollWidth - window.innerWidth); 
-
-  gsap.fromTo(thisAnimWrap, { 
-    x: () => thisAnimWrap.classList.contains('animation-wrap') ? 0 : getToValue() }, { 
-    x: () => thisAnimWrap.classList.contains('animation-wrap') ? getToValue() : 0, 
-    ease: "none",
-    scrollTrigger: {
-      trigger: sec,		
-      start: "top top",
-      end: () => "+=" + thisAnimWrap.scrollWidth,
-      pin: true,  
-	     pinReparent: true, 
-      invalidateOnRefresh: true,
-      scrub: true,
-    }
-  });
-
-});	
 
 
 
-// split text titles
-  // var text = gsap.utils.toArray(".aniText");
-  // text.forEach((el) => {
-  //     var splitWords = new SplitText(el, {type: "words,chars"});
-  //     chars = splitWords.chars;
-
-  //     var splitTimeline = gsap.timeline({
-  //       scrollTrigger: {
-  //         trigger: el,
-  //         start: "top bottom-=100",
-  //         end: "bottom top",
-  //         toggleActions: "play none none none",
-  //       }
-  //     });
-  //     splitTimeline.from(chars, {
-  //         opacity:0,
-  //         duration: 0.5,
-  //         yPercent: 10,
-  //         rotate:30,
-  //         ease: "back.out",
-  //         stagger: 0.02
-  //     });
-  // });
-
-  ///////////////////////////////////////////
-
-
-////////////////////////---------- Fixed section
-var scrollUp = document.querySelector('.navbar');
-
-////adds bg color when start scrolling
-ScrollTrigger.create({
-  id:'scrolling-down',
-  start: 'top top-=50',
-  end:'+=21000',
-  toggleClass: {className: 'nav--scrolled', targets: scrollUp,},
-});
 
 
 
-/////////////////////////////text feature
-
-// Rate = Distance over Time r=d/t
-// If we want to define the rate, and 
-// the distance is determined, 
-// time will have to be variable
-
-// We want to define the rate, and we can 
-// define that statically
-r = 50;
-adjustJank = 4; // Set this to 0 to see the jank I'm talking about ... this just adds to the distance animated to smooth out the seam
-
-// Get the initial scroll elements and save them for later
-const scrollElems = document.querySelectorAll('.marquee-container p');
-
-// Adjust our tween based on the object and distance given
-function adjustTween(obj, d) {
-  // Get the progress of the previous tween if it exists
-  let progress = 0;
-  if(obj.tween) {
-    progress = obj.tween.progress();
-    // Kill the previous tween
-    obj.tween.kill();
-  }
-    
-  // r = d/t 
-  // r*t = d
-  // t = d/r
-  
-  // Set the proper time
-  var t = d/r;
-
-  // Create a new tween to animate our text so that it loops
-  // Make sure to save it to the object so we can refer to it later
-  obj.tween = gsap.fromTo(obj.parentElement, {x: 0}, {
-    duration: t,
-    x: "-"+(d+adjustJank), 
-    ease: 'linear',
-    repeat: -1,
-  }).progress(progress); // Set the progress of the new tween to the same value of
-                         // the previous tween (if it exists) before it was killed
-}
-
-// Set up for what appears to be an seamless stream of text
-// This could go in an init() function
-scrollElems.forEach((obj, i) => {
-  var d = obj.offsetWidth;
-  var parent = obj.parentElement;
-  var clone = obj.cloneNode(true);
-  parent.appendChild(clone);
-  gsap.set(parent.parentElement, {width: d});
-  
-  adjustTween(obj, d);
-});
-
-// Adjust widths and tweens on resize
-window.addEventListener("resize", () => {
-  scrollElems.forEach((obj, i) => {
-    var d = obj.offsetWidth;
-    var parent = obj.parentElement;
-    gsap.set(parent.parentElement, {width: d});
-    adjustTween(obj, d);
-  });
-});
-
-
-// =============================
