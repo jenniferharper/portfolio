5853f0455560502eb94f160227514b7806ed1bb0
jenniferHarper <52581347+jenniferharper@users.noreply.github.com>
2022-03-26 20:10:05 +1300

    once more

--- yogaStudio/js/script.js
+++ yogaStudio/js/script.js
@@ -68,9 +68,9 @@ ScrollTrigger.matchMedia({
     gsap.to(".drawText",10,{attr:{startOffset:'105%'},repeat:-1, ease:'none'});
 
     //// set image to center on masked images
-    gsap.set('#about-panning, #horz-one-panning, #horz-two-panning', {	
-      attr:{ x:'-50%' },
-    })
+    // gsap.set('#about-panning, #horz-one-panning, #horz-two-panning', {	
+    //   attr:{ x:'-50%' },
+    // })
 
     let heroPan = gsap.timeline({repeat:-1, yoyo:true,}); 
     heroPan.to('.bg-img-hero', { backgroundPosition:'90% 100%', duration:15, ease:'none'})
