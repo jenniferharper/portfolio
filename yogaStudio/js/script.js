51c659c1ee876e63938d5d8630a60f5222b06176
jenniferHarper <52581347+jenniferharper@users.noreply.github.com>
2022-03-26 19:14:20 +1300

    alert

--- yogaStudio/js/script.js
+++ yogaStudio/js/script.js
@@ -142,7 +142,7 @@ ScrollTrigger.matchMedia({
 
     } else {
       ///-- This is for touch devices ---//////
-      console.log('tablet/mobile')
+      alert('tablet/mobile')
 
       ////---------------deactivate slider
       $( ".slider" ).addClass( "touch" ); 
